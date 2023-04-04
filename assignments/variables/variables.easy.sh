1ef68ae190b459639e7b504fbfcbff90996b0386
Yvon Momboisse <yvon.momboisse@knap.fr>
2023-04-04 15:27:23 +0200

    variable assignments (wip)

---
+++ assignments/variables/variables.easy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# assignment 1
+greeting="Hello, world!"
+echo $greeting
+
+# assignment 2
+x=5
+y=10
+
+z=$((x+y))
+
+echo $z
+
+
+# assignment 3
+
+fruits=("apple" "banana" "orange")
+echo ${fruits[1]}
